22c2d2b9c6cf8b700930768f3d74410cf4f0fe4c
YEAHYEAHYEAHYEAH <ewanmorrison@hotmail.co.uk>
2018-03-18 20:25:59 +0000

    Add files via upload

--- EquilibriumTime.py
+++ EquilibriumTime.py
@@ -18,42 +18,47 @@ import scipy.odr as odr
 
 
 def equilibriumRange(xData):
-    equibVal = np.mean(xData[10000:])
+    equibVal = np.mean(xData[20000:])
+    
+    counter = 0
     endRange = 0
     for i in xrange(len(xData)):
-        if abs(equibVal - xData[i])<0.05:
+        if abs(equibVal - xData[i])<0.04:
             endRange = i
             break
-    pl.plot(xData[:endRange])
-    pl.show()
-
+    #pl.plot(xData[:endRange])
+    #pl.show()
+    return endRange
 
 def findEquilibriumTime():
     
-    resFile = open("EquilibriumTime.txt",'w')
-    resFile.write("500 beads\nTotal Lk,Equilibrium Time,Error\n\n")
+    Lks = []
+    equibTimes = []
     
     for fname in glob.glob('data/*'):
         title = fname[fname.find("/")+1:-4]
         dataFile = open(fname,'r')
         plotArray = np.genfromtxt(dataFile,delimiter=',',skip_header=3)
-        
+
+        initialLk =  int(title[5:-4])
         frameNo = plotArray[:,0]
         twists = plotArray[:,1]
         writhes = plotArray[:,2]        
         total = plotArray[:,1]+plotArray[:,2]
         
         equibTime = equilibriumRange(twists)
+        equibTimes.append(equibTime)
+        Lks.append(initialLk)
+    pl.plot(Lks,equibTimes,  color="k", label="Twists" ,marker=".", linestyle=" ")
+    pl.xlabel("Initial Twist")
+    pl.ylabel("Equilibrium Time")
+    pl.title("Average Equilibrium Times for Different Initial Twists")
+
+    pl.savefig("EquilibriumTime", dpi=300, facecolor='w', edgecolor='w',
+               orientation='landscape', papertype=None, format=None,
+               transparent=False, bbox_inches='tight', pad_inches=0.01,
+               frameon=None)
+    pl.close()
         
-        #pl.plot(frameNo,twists,  color="r", label="Twists" ,marker="o", linestyle=" ")
-        #pl.plot(frameNo,writhes, color="b", label="Writhes",marker="o", linestyle=" ")
-        #pl.plot(frameNo,total,   color="k", label="Total"  ,marker="o", linestyle=" ")
-        #pl.xlabel("Frame Number")
-        #pl.ylabel("Total Linking Number")
-        #pl.title(title)
-        #pl.legend(loc="best")
-        #pl.savefig(title, dpi=300, facecolor='w', edgecolor='w',
-        #        orientation='landscape', papertype=None, format=None,
-        #        transparent=False, bbox_inches='tight', pad_inches=0.01,
-        #        frameon=None)
-        #pl.close()
+        
+findEquilibriumTime()
